fd9a10a99dd5166ba20d2507843b8b760f80800a
Paul Thomson <paul.thomson330@gmail.com>
2026-04-14 19:53:11 -0400

    feat: implement InMemoryEventRepository with findEventById

--- src/repository/EventRepository.ts
+++ src/repository/EventRepository.ts
@@ -1,8 +1,33 @@
 import type { Result } from "../lib/result";
 import type { EventError } from "../lib/errors";
 import type { IEventRecord } from "../lib/event";
+import {Ok, Err} from "../lib/result";
+import {
+  EventNotFound,
+  UnexpectedDependencyError,
+} from "../lib/errors";
  
  
 export interface IEventRepository {
   findEventById(id: string): Promise<Result<IEventRecord | null, EventError>>;
+}
+
+export const eventStorage: IEventRecord[] = [];
+
+class InMemoryEventRepository implements IEventRepository {
+
+  async findEventById(
+    id: string,
+  ): ReturnType<IEventRepository["findEventById"]> {
+    try {
+      const match = eventStorage.find((e) => e.id === id) ?? null;
+      return Ok(match);
+    } catch {
+      return Err(UnexpectedDependencyError("Failed to read events."));
+    }
+  }
+}
+
+export function CreateInMemoryEventRepository(): IEventRepository {
+  return new InMemoryEventRepository();
 }
